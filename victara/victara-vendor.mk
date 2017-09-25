884192baabcafbcb17bb631c9f136147d37f2205
YumeMichi <do4suki@gmail.com>
2017-09-25 09:40:16 +0800

    victara: Add support for aptX & aptX HD
    Change-Id: I03d4dbf158b4657e3a2fb6fdac1460104781b78f
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- victara/victara-vendor.mk
+++ victara/victara-vendor.mk
@@ -164,6 +164,8 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/victara/proprietary/vendor/lib/libadiertac.so:system/vendor/lib/libadiertac.so \
     vendor/motorola/victara/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
     vendor/motorola/victara/proprietary/vendor/lib/libadsprpc.so:system/vendor/lib/libadsprpc.so \
+    vendor/motorola/victara/proprietary/vendor/lib/libaptXHD_encoder.so:system/vendor/lib/libaptXHD_encoder.so \
+    vendor/motorola/victara/proprietary/vendor/lib/libaptX_encoder.so:system/vendor/lib/libaptX_encoder.so \
     vendor/motorola/victara/proprietary/vendor/lib/libaudcal.so:system/vendor/lib/libaudcal.so \
     vendor/motorola/victara/proprietary/vendor/lib/libbccQTI.so:system/vendor/lib/libbccQTI.so \
     vendor/motorola/victara/proprietary/vendor/lib/libbtnv.so:system/vendor/lib/libbtnv.so \
